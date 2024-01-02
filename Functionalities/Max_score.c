2ebf92af3bd2c52d52f291ee94e6158ae582e416
Umang8251 <umangsinghal8251@gmail.com>
2024-01-02 16:15:15 +0530

    Quiz Game using DSA in C

---
+++ Functionalities/Max_score.c
@@ -0,0 +1,30 @@
+//WHY MAX HEAP?
+/*1)Constant Time Retrieval: In a max-heap, the maximum element is always at the root. Thus, finding the maximum
+element involves a simple lookup operation at the root, resulting in constant time complexity (O(1)). In 
+contrast, searching for the maximum element in an unsorted array would require linear time (O(n)).
+2)Space Efficiency: Max-heaps can be implemented using an array, providing a space-efficient representation. 
+Each element in the array corresponds to a node in the heap, and the structure of the heap allows for efficient
+storage.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Max_score.h"
+void max_score(int* scores) {
+    
+
+    // Calculate the number of elements in the array
+    int numElements = 3;
+
+    // Initialize the maximum score with the first element of the array
+    int maxScore = scores[0];
+
+    // Iterate through the array to find the maximum score
+    for (int i = 1; i < numElements; i++) {
+        if (scores[i] > maxScore) {
+            maxScore = scores[i];
+        }
+    }
+
+    // Print the maximum score
+    printf("The highest score is: %d\n", maxScore);
+}
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Quiz Game
- This is a Quiz Game programmed in C language, designed using various data structures such as Stacks ,Priority Queue, Maximum Heap. It is a project from 3rd semester of PES University, Bengaluru under course Data Structures and its Applications.
+This is a Quiz Game programmed in C language, designed using various data structures such as Stacks ,Priority Queue, Maximum Heap. It is a project from 3rd semester of PES University, Bengaluru under course Data Structures and its Applications.
+
+The files have to be run with the make -f palin_make.mk command on the command prompt followed by a.exe command.
+
+Instruction to install make: https://linuxhint.com/run-makefile-windows/
+
+Requirements(hardware and software) :
+
+To run this program you will require :
+
+Hardware: A fully-functional computer system with all basic hardware such as mouse, keyboard, etc.
+ 
+Software: The latest and updated version of C, with an appropriate compiler for C such as gcc, and an interface such as Microsoft Visual Studio Code or Notepad++ to view the source code of the program.
+
 
